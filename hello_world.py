e9678b82242bf445f5972ff2c0ebd29b281f2559
Kwang Wei Tan <s10186878@connect.np.edu.sg>
2020-11-04 21:30:23 +0800

    Hello again. I am here to add in a second line. I am Lennon

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,3 @@
 print('Hello World!')
 #“This is hello from <insert your developer name here>. I am adding a new line of code here.”
+#“Hello again. I am here to add in a second line. I am <insert your developer name here>”
